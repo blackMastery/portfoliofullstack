83c907b8f335201f7fb70ea782f0179dce89a38b
blackMastery <black.king1232@gmail.com>
2020-07-02 07:47:52 -0400

    deploy test..

--- portfolio/config/pro.py
+++ portfolio/config/pro.py
@@ -15,7 +15,7 @@ ADMINS = (
     )
 
 
-ALLOWED_HOSTS = ['https://shrouded-woodland-74637.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['shrouded-woodland-74637.herokuapp.com', '127.0.0.1']
 
 # DATABASES = {
 #     'default': {
